f61417aa5315dc8b99e16f40bd48b782afd30ac9
Tyler Barber <dev@twbarber.com>
2016-12-08 08:27:25 -0600

    Added function structure for script outline

--- setup.sh
+++ setup.sh
@@ -1,3 +1,25 @@
 #!/bin/bash
 
 echo Starting Setup...
+
+docker() {
+
+}
+
+applications() {
+
+}
+
+theme() {
+
+}
+
+dotfiles() {
+
+}
+
+echo ""
+echo "==============="
+echo " PLEASE REBOOT "
+echo "==============="
+echo ""
